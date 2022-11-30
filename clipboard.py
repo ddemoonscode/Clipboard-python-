b9904fb288708bb8f3047f52ff2b20f97b0dad2b
ddemoon's code <69145435+ddemoonscode@users.noreply.github.com>
2022-11-30 09:30:08 +0100

    Add files via upload

--- clipboard.py
+++ clipboard.py
@@ -1,15 +1,22 @@
 import pyautogui
 
 import time
-
+print()
 #input
 word = input('Sentence: ')
 num = int(input('how often?: '))
 
 #Time till start after input
-time.sleep(3)
+print('3')
+time.sleep(1)
+print('2')
+time.sleep(1)
+print('1')
+time.sleep(1)
+print('Go')
+
 #How often typed
-for i in range(num):
+for _ in range(num):
     #message
     pyautogui.typewrite(word)
     #press enter
